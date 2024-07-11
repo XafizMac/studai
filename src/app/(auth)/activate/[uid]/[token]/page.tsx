ce7403a7abfcb56c9ca47b63313d98c262e13fa4
Жолдошов Абдулхафиз <guest1@MacBook-Pro-shama.local>
2024-07-11 16:43:24 +0600

    надеюсь last commit

--- src/app/(auth)/activate/[uid]/[token]/page.tsx
+++ src/app/(auth)/activate/[uid]/[token]/page.tsx
@@ -1,82 +1,28 @@
-"use client";
+import dynamic from 'next/dynamic';
 
-import { useParams, useRouter } from 'next/navigation';
-import styles from "./token.module.scss"
-import { useContext, useEffect, useState } from 'react';
-import { Context } from '@/app/clientProvider';
-import { Progress } from 'antd';
-import { GetStaticPaths, GetStaticProps } from 'next';
+const VerifyPage = dynamic(() => import('./client-page'), { ssr: false });
 
-
-interface VerifyPageProps {
-  uid: string;
-  token: string;
+interface PageProps {
+  params: {
+    uid: string;
+    token: string;
+  };
 }
 
-const VerifyPage = () => {
-  const params = useParams();
-  const { uid, token } = params as { uid: string, token: string };
-  const { store } = useContext(Context)
-  const { replace } = useRouter();
-  const [count, setCount] = useState<number>(0);
-
-  useEffect(() => {
-    activateAccount()
-    setInterval(() => setCount(prev => prev + 1), 100)
-  }, [store])
-
-  if (!uid || !token) {
-    return <div>Loading...</div>;
-  }
-
-  const activateAccount = async () => {
-    try {
-      store.activation(uid, token)
-      setTimeout(goToLogin, 5000);
-    }
-    catch (error) {
-    }
-  }
-
-  const goToLogin = () => {
-    replace('/auth/login');
-  }
-
-  return (
-    <div className={styles.main}>
-      <div className="container">
-        <div className={styles.main_row}>
-          <p className={styles.title}>Активация аккаунта</p>
-          <Progress
-            type='circle'
-            percent={count}
-            format={(percent) => `${percent} %`}
-            strokeColor={""}
-            trailColor='white'
-            strokeLinecap='round'
-          />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
+export async function generateStaticParams() {
   const paths = [
-    { params: { uid: 'some-uid', token: 'some-token' } },
+    { uid: 'some-uid', token: 'some-token' },
   ];
 
-  return { paths, fallback: 'blocking' };
+  return paths.map(({ uid, token }) => ({
+    uid,
+    token,
+  }));
+}
+
+const Page = ({ params }: PageProps) => {
+  const { uid, token } = params;
+  return <VerifyPage uid={uid} token={token} />;
 };
 
-export const getStaticProps: GetStaticProps<VerifyPageProps> = async ({ params }) => {
-  const { uid, token } = params as { uid: string; token: string };
-  return {
-    props: {
-      uid,
-      token,
-    },
-  };
-};
-
-export default VerifyPage;
+export default Page;
--- src/app/(auth)/password/reset/confirm/[uid]/[token]/page.tsx
+++ src/app/(auth)/password/reset/confirm/[uid]/[token]/page.tsx
@@ -1,124 +1,28 @@
-"use client";
+import dynamic from 'next/dynamic';
 
-import Image from "next/image";
-import styles from "./resetpasswordconfirm.module.scss";
-import logo from "../../../../../../../../public/logo.svg";
-import type { FormProps } from "antd";
-import { Button, Form, Input, message } from "antd";
-import Link from "next/link";
-import { motion } from "framer-motion";
-import { useContext, useState } from "react";
-import { useParams, useRouter } from "next/navigation";
-import { Context } from "@/app/clientProvider";
-import axios from "axios";
-import { UserOutlined } from "@ant-design/icons";
+const ResetPasswordConfirm = dynamic(() => import('./client-page'), { ssr: false });
 
-type FieldType = {
-  newPassword: string;
+interface PageProps {
+  params: {
+    uid: string;
+    token: string;
+  };
+}
+
+export async function generateStaticParams() {
+  const paths = [
+    { uid: 'some-uid', token: 'some-token' },
+  ];
+
+  return paths.map(({ uid, token }) => ({
+    uid,
+    token,
+  }));
+}
+
+const Page = ({ params }: PageProps) => {
+  const { uid, token } = params;
+  return <ResetPasswordConfirm uid={uid} token={token} />;
 };
 
-export default function ResetPasswordConfirm() {
-  const params = useParams();
-  const { uid, token } = params as { uid: string; token: string };
-  const { store } = useContext(Context);
-  const { push, replace } = useRouter();
-  const [messageApi, contextHolder] = message.useMessage();
-  const [loading, setLoading] = useState<boolean>(false);
-
-  const onFinish: FormProps<FieldType>["onFinish"] = async (values) => {
-    setLoading(true);
-    try {
-      const result = await store.resetPasswordConfirm(
-        uid,
-        token,
-        values.newPassword
-      );
-      if (store.status.status == 204) {
-        success();
-        push("/auth/login");
-      } else {
-        error(store.status.statusText);
-      }
-    } catch (e) {
-      error("Не удалось подключиться к интернету");
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const error = (message?: string) => {
-    messageApi.open({
-      type: "error",
-      content: message || "Ошибка при сбросе пароля",
-    });
-  };
-
-  const success = () => {
-    messageApi.open({
-      type: "success",
-      content: "Ваш пароль обновлен",
-    });
-  };
-
-  return (
-    <div className={styles.main}>
-      {contextHolder}
-      <div className={styles.form}>
-        <Link href={"/"} className={styles.logo}>
-          <Image width={60} src={logo} alt="Logo" />
-          <p className="active">Studai</p>
-        </Link>
-        <p className={styles.title}>Создание пароля</p>
-        <Form
-          name="basic"
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          autoComplete="off"
-          className={styles.forms}
-        >
-          <motion.div
-            initial={{ opacity: 0, height: 0 }}
-            animate={{ opacity: 1, height: "100%" }}
-            transition={{ duration: 0.2 }}
-          >
-            <Form.Item<FieldType>
-              rules={[
-                {
-                  required: true,
-                  message: "Пожалуйста, введите новый пароль!",
-                },
-                {
-                  pattern:
-                    /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                  message:
-                    "Пароль должен содержать не менее 8 символов, включая заглавные и строчные буквы, цифру и специальный символ!",
-                },
-              ]}
-              name="newPassword"
-            >
-              <Input.Password
-                prefix={<UserOutlined />}
-                type="password"
-                placeholder="Введите новый пароль"
-                size="large"
-              />
-            </Form.Item>
-          </motion.div>
-          <Form.Item>
-            <Button
-              loading={loading}
-              className={styles.btn}
-              size="large"
-              type="primary"
-              htmlType="submit"
-            >
-              Сбросить пароль
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
-      {/* Image */}
-      <div className={styles.background}></div>
-    </div>
-  );
-}
+export default Page;
