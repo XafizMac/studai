9a100fffc64a08ff71874f983760c388339e1a3c
Жолдошов Абдулхафиз <guest1@MacBook-Pro-shama.local>
2024-07-14 02:40:54 +0600

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={exo_2.className}>
-        {/* <ClientProvider> */}
-          <div>Hello Next JS</div>
-        {/* </ClientProvider> */}
+        <ClientProvider>
+          {children}
+        </ClientProvider>
       </body>
     </html>
   );
