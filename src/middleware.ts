f4b1c955ffdcd0b54877b9cf8768db8de40b1085
Жолдошов Абдулхафиз <guest1@MacBook-Pro-shama.local>
2024-07-13 21:54:09 +0600

    commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,5 +21,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/', '/dashboard', '/dashboard/:path*', '/auth/:path*'],
+    matcher: ['/', '/dashboard', '/dashboard/:path*', '/auth'],
 };
