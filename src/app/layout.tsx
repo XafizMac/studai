abe10c800220161a60100a0ee882c16058179478
Жолдошов Абдулхафиз <guest1@MacBook-Pro-shama.local>
2024-07-14 21:53:20 +0600

    mycommit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={exo_2.className}>
-        <ClientProvider>
+        {/* <ClientProvider>
           {children}
-        </ClientProvider>
+        </ClientProvider> */}
+        Hello
       </body>
     </html>
   );
