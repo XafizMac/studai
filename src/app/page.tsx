6bd54583af67e5c04cdba6cf0b403809dcbd005b
Жолдошов Абдулхафиз <guest1@MacBook-Pro-shama.local>
2024-07-15 00:03:43 +0600

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,65 +10,60 @@ import { Pricing } from "@/components/ui/pricing/Pricing";
 import { Footer } from "@/components/ui/footer/Footer";
 import { Spin } from "antd";
 import { useSearchParams } from "next/navigation";
+import { Context } from "./clientProvider";
 
-// export default function Home() {
-//   const { store } = useContext(Context);
-//   const [loading, setLoading] = useState(true);
-//   const searchParams = useSearchParams();
+export default function Home() {
+  const { store } = useContext(Context);
+  const [loading, setLoading] = useState(true);
+  const searchParams = useSearchParams();
 
-//   const state = searchParams.get("state");
-//   const code = searchParams.get("code");
+  const state = searchParams.get("state");
+  const code = searchParams.get("code");
 
-//   if (state && code) {
-//     console.log("State: ", state);
-//     console.log("Code: ", code);
+  if (state && code) {
+    console.log("State: ", state);
+    console.log("Code: ", code);
 
-//     const OAuthCallback = async () => {
-//       try {
-//         const response = await store.oAuthCallbacks(state, code);
-//         console.log(response);
-//       } catch (err) {
-//         console.log("Error", err);
-//       }
-//     };
-//     OAuthCallback();
-//   }
+    const OAuthCallback = async () => {
+      try {
+        const response = await store.oAuthCallbacks(state, code);
+        console.log(response);
+      } catch (err) {
+        console.log("Error", err);
+      }
+    };
+    OAuthCallback();
+  }
 
-//   useEffect(() => {
-//     const token = localStorage.getItem("token");
-//     if (!token) {
-//       store.checkAuth().finally(() => {
-//         setLoading(false);
-//       });
-//     } else {
-//       setLoading(false);
-//     }
-//   }, [store]);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      store.checkAuth().finally(() => {
+        setLoading(false);
+      });
+    } else {
+      setLoading(false);
+    }
+  }, [store]);
 
-//   if (loading) {
-//     return (
-//       <div>
-//         <Spin fullscreen={true} size="large" />
-//       </div>
-//     );
-//   }
+  if (loading) {
+    return (
+      <div>
+        <Spin fullscreen={true} size="large" />
+      </div>
+    );
+  }
 
-//   return (
-//     <main className="mainPage">
-//       <p>Hello</p>
-//       {/* <Navbar />
-//       <MainPage />
-//       <Services />
-//       <Choises />
-//       <Instruction />
-//       <Pricing />
-//       <Footer /> */}
-//     </main>
-//   );
-// }
-
-export default function mainPage() {
-  return <div>
-    <p>Home</p>
-  </div>;
+  return (
+    <main className="mainPage">
+      <p>Hello</p>
+      {/* <Navbar />
+      <MainPage />
+      <Services />
+      <Choises />
+      <Instruction />
+      <Pricing />
+      <Footer /> */}
+    </main>
+  );
 }
