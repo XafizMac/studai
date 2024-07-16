d85edb93b687834ba5a4b54a4e76ec5d056e1093
Жолдошов Абдулхафиз <guest1@MacBook-Pro-shama.local>
2024-07-16 20:41:02 +0600

    vercel

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   const response = NextResponse.json({ message: 'Logged in' });
-  response.cookies.set('token', 'logged in', { 
+  response.cookies.set('tkn', 'logged in', { 
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development',
     path: '/',
--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   const response = NextResponse.json({ message: 'Logged out' });
-  response.cookies.delete('token');
+  response.cookies.delete('tkn');
   return response;
 }
 
